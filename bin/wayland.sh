3be8dcfe68c6d29aa3891b5c6703b7abdfcdf1a8
moatx <wowzers1lol@gmail.com>
2022-12-31 04:50:01 +0300

    initial commit

---
+++ bin/wayland.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd ~ || exit
+export _JAVA_AWT_WM_NONREPARENTING=1
+export XCURSOR_SIZE=24
+
+export QT_QPA_PLATFORM=wayland
+
+export SDL_VIDEODRIVER=wayland
+
+export CLUTTER_BACKEND=wayland
+export GTK_THEME=Adwaita:dark
+export GDK_BACKEND=wayland
+
+dbus-daemon --session --address=unix:path="$XDG_RUNTIME_DIR"/bus &
+exec $1
+# Hyprland
+
