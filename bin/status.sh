c7df8dd5a7d3f3f54419b56016c9b771de734491
moatx <wowzers1lol@gmail.com>
2023-07-10 23:07:49 +0300

    Mon Jul 10 23:07:49 +03 2023

--- bin/status.sh
+++ bin/status.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+
+memtotal="$(mem -t)"
+
 while :; do
-   echo "battery: $(power) | temp: $(temp) | usedMem: $(mem -u) | date: $(date +"%F") | time: $(date +"%I:%M:%S") "
-   sleep 1.5s
+   #printf "%s" "battery: $(power) | temp: $(temp) | usedMem: $(mem -u) | date: $(date +"%F") | time: $(date +"%I:%M:%S") "
+   #
+   if [ "$(power -c)" = "yes" ] 
+   then
+	   charging_status="charging"
+   else
+	  charging_status="discharging"
+   fi
+
+   echo "batt: $(power) ($charging_status) | temp: $(temp) | mem: $(mem -u) / $memtotal | $(date +"%F") $(date +"%I:%M:%S") "
 done 
 
